7e688f71c8aaa8008b0dfabc665a09a486052405
aro28 <prostotak28@outlook.com>
2023-05-29 14:18:18 +0600

    added allowance of creation Student and Tutor with seversal instances&or empty

--- users_app/serializers.py
+++ users_app/serializers.py
@@ -6,10 +6,20 @@ from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
+from django.db import transaction
 
+class TutorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tutor
+        fields = '__all__'
+
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        fields = '__all__'
 
 class StudentRegisterSerializer(serializers.ModelSerializer):
-    tutors = serializers.PrimaryKeyRelatedField(many=True, queryset=Tutor.objects.all())
+    tutors = serializers.PrimaryKeyRelatedField(many=True, queryset=Tutor.objects.all(), allow_empty=True, required=False)
     password = serializers.CharField(max_length=20, write_only=True)
     password2 = serializers.CharField(max_length=20, write_only=True)
 
@@ -29,39 +39,39 @@ class StudentRegisterSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError("Password must contain both uppercase and lowercase letters")
         return data
 
+    @transaction.atomic
     def create(self, validated_data):
-        user = User(
-            username=validated_data['username'],
-            first_name=validated_data['first_name'],
-            last_name=validated_data['last_name'],
-            age=validated_data['age']
-        )
-        profile_image = validated_data.get('profile_image')
-        if profile_image:
-            user.profile_image = profile_image
-        user.set_password(validated_data['password'])
+        tutors_data = validated_data.pop('tutors',[])
+        password = validated_data.pop('password')
+        password2 = validated_data.pop('password2')
+        user = User.objects.create(**validated_data)
+        user.set_password(password)
         user.save()
-        try:
-            student = Student.objects.create(user=user)
-        except Exception as e:
-            user.delete()
-            raise e
+        if tutors_data:
+            voted_tutor = tutors_data[0]
+            print(f"voted_student: {voted_tutor}")
         else:
-            student.username = user.username
-            student.profile_image = user.profile_image
-            student.age = user.age
+            voted_tutor = None
+
+        student = Student.objects.create(user=user, voted_tutor=voted_tutor)
+        student.tutors.set(tutors_data)
+        student.username = user.username
+        student.profile_image = user.profile_image
+        student.age = user.age
+        student.save()
         return student
 
 
 class TutorRegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(max_length=20, write_only=True)
     password2 = serializers.CharField(max_length=20, write_only=True)
-    experience = serializers.CharField(max_length=100)
+    experience = serializers.CharField(max_length=100, allow_null=True)
+    students = serializers.PrimaryKeyRelatedField(many=True, queryset=Student.objects.all(), allow_empty=True, required=False)
 
     class Meta:
         model = User
         fields = ["username", "password", "password2",
-                  "first_name", "last_name", "profile_image", "age", "experience"]
+                  "first_name", "last_name", "profile_image", "age", "experience", "students"]
 
     def validate(self, data):
         if data['password'] != data['password2']:
@@ -81,45 +91,33 @@ class TutorRegisterSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError("You couldn't start working before the age og 18!")
         return data
 
+    @transaction.atomic
     def create(self, validated_data):
-        user = User(
-            username=validated_data['username'],
-            first_name=validated_data['first_name'],
-            last_name=validated_data['last_name'],
-            age=validated_data['age']
-        )
-        profile_image = validated_data.get('profile_image')
-        if profile_image:
-            user.profile_image = profile_image
-        user.set_password(validated_data['password'])
+        students_data = validated_data.pop('students', [])
+        print(f"students_data: {students_data}")
+        password = validated_data.pop('password')
+        password2 = validated_data.pop('password2')
+        experience = validated_data.pop('experience')
+        user = User.objects.create(**validated_data)
+        user.set_password(password)
         user.save()
-        try:
-            tutor = Tutor.objects.create(user=user, experience=validated_data['experience'])
-        except Exception as e:
-            user.delete()
-            raise e
+        if students_data:
+            voted_student = students_data[0]
+            print(f"voted_student: {voted_student}")
         else:
-            tutor.username = user.username
-            tutor.profile_image = user.profile_image
-            tutor.age = user.age
+            voted_student = None
+        tutor = Tutor.objects.create(user=user, experience=experience)
+        tutor.students.set(students_data)
+        tutor.username = user.username
+        tutor.profile_image = user.profile_image
+        tutor.age = user.age
+        tutor.save()
         return tutor
 
     @receiver(post_save, sender=settings.AUTH_USER_MODEL)
     def create_auth_token(sender, instance=None, created=False, **kwargs):
         if created:
             Token.objects.create(user=instance)
-
-
-class TutorSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Tutor
-        fields = '__all__'
-
-
-class StudentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Student
-        fields = '__all__'
 
 
 class VoteForTutorSerializer(serializers.ModelSerializer):
